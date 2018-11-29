6e097035c60225e09007c062ea3a6b20c971fd6d
goenagaj <43122583+goenagaj@users.noreply.github.com>
2018-11-28 19:49:24 -0500

    Update Library.h

--- Project1/Library.h
+++ Project1/Library.h
@@ -1,27 +1,25 @@
 #pragma once
+
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class Library
+
 {
+
 public:
 
-	Library (string address, string name, string customers, string phone_number, string librarians, string books);
+	Library(string address, string name, string phone_number);
 	string getaddress();
 	string getphoneNumber();
-	string getlibrarians();
-	string getbooks();
 	string getname();
-	string getaddress();
 
-	void checkOut();
-	void checkIn();
-	void searchBook();
 
 	~Library();
 
 private:
-	
 	string name;
 	string address;
 	string phone_number;
