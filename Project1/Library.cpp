b53081a1d3b6515ce75c9ef1e463c1b8fd8627cf
goenagaj <43122583+goenagaj@users.noreply.github.com>
2018-11-28 19:51:14 -0500

    Update Library.cpp

--- Project1/Library.cpp
+++ Project1/Library.cpp
@@ -1,16 +1,28 @@
-#include "Libary.h"
+#include "library.h"
+#include <iostream>
 #include <string>
 
+using namespace std;
 
-Libary::Libary()
-{
-	sting books;
-	string employees;
-	string phoneNumber;
+Library::Library(string address, string name, string phone_number) {
+
+	address = a;
+	name = n;
+	phone_number = p;
+
 }
 
-
-Libary::~Libary()
+string Library::getaddress()
 {
+	return address;
+}
 
+string Library::getphoneNumber()
+{
+	return phone_number;
+}
+
+string Library::getname()
+{
+	return name;
 }
